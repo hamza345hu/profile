42727937934a5fea4e3d40d99b8b5143f55f6c8d
unknown <hamzadujaily345@gmail.com>
2022-01-04 14:50:57 -0800

    add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,51 @@
+//##################################################################
+//box-sitting
+let boxSitting=document.querySelector('.box-sitting')
+let iconSitting=document.querySelector('.toggle i');
+iconSitting.onclick=()=>{
+    boxSitting.classList.toggle('open')
+}
+// color siting
+let styleC=document.querySelectorAll('.style');
 
+styleC.forEach(function (s) {
+    s.classList.remove('active');
+})
+styleC.forEach(function(s,index){
+    let styleColors=Array.from(document.querySelectorAll(`.colors${index+1} .color`)); 
+    s.onclick=function () { 
+        styleActive();
+        switchColors('--bgColor-1', styleColors,0);
+        switchColors('--bgColor-2', styleColors,1);
+        switchColors('--primary-color', styleColors,2);
+        switchColors('--light-color', styleColors,3);
+        addLocal();
+    };
+    function styleActive() {
+        styleC.forEach((st)=>{
+            st.classList.remove('active');
+        })
+        s.classList.add('active');
+    }
+    function addLocal() {
+        let addArr=[];
+        for (let i = 0; i < styleColors.length; i++) {
+          addArr.push(styleColors[i].dataset.color);   
+        }
+        window.localStorage.setItem('colors',JSON.stringify(addArr)) 
+    };
+    //get from local storage
+    let getLocal=JSON.parse(window.localStorage.getItem('colors'));
+    document.documentElement.style.setProperty('--bgColor-1',getLocal[0]);
+    document.documentElement.style.setProperty('--bgColor-2',getLocal[1]);
+    document.documentElement.style.setProperty('--primary-color',getLocal[2]);
+    document.documentElement.style.setProperty('--light-color',getLocal[3]);
+    });
+
+function switchColors(cssColor,array,n) {
+    document.documentElement.style.setProperty(cssColor,array[n].dataset.color);
+}
+//#############################################################################################
 // scroll to top for website
 let up=document.querySelector('.up');
 window.onscroll=function () {
@@ -16,7 +63,7 @@ up.addEventListener('click',function () {
         behavior: "smooth"
     })
 })
-
+//#############################################################################
 // add class active to nav link.....
 let navLink=document.querySelectorAll('.nav-link');
 navLink.forEach(function (ele) {
@@ -29,11 +76,11 @@ navLink.forEach(function (ele) {
         ele.classList.add('navLink-active')
     })
 })
-
+//################################################################################
 // let about=document.querySelector(".about");
 // let aboutText=document.querySelector(".about-text");
 // window.onscroll=function () {
 //     if (window.scrollY >= about.offsetTop) {
 //         aboutText.style.right= 0;
-//     }
-// }
+//     }}
+
