b700cae8e710f7dafb6032c4ef232072cc1776f7
unknown <hamzadujaily345@gmail.com>
2022-03-04 05:47:32 -0800

    add

--- js/main.js
+++ js/main.js
@@ -1,58 +1,10 @@
-//##################################################################
-//box-sitting
-// let boxSitting=document.querySelector('.box-sitting')
-// let iconSitting=document.querySelector('.toggle i');
-// iconSitting.onclick=()=>{
-//     boxSitting.classList.toggle('open')
-// }
-// // color siting and Add to local storage
-// let styleC=document.querySelectorAll('.style');
-
-// styleC.forEach(function (s) {
-//     s.classList.remove('active');
-// })
-// styleC.forEach(function(s,index){
-//     let styleColors=Array.from(document.querySelectorAll(`.colors${index+1} .color`)); 
-//     s.onclick=function () { 
-//         styleActive();
-//         switchColors('--bgColor-1', styleColors,0);
-//         switchColors('--bgColor-2', styleColors,1);
-//         switchColors('--primary-color', styleColors,2);
-//         switchColors('--light-color', styleColors,3);
-//         addLocal();
-//     };
-//     function styleActive() {
-//         styleC.forEach((st)=>{
-//             st.classList.remove('active');
-//         })
-//         s.classList.add('active');
-//     }
-//     function addLocal() {
-//         let addArr=[];
-//         for (let i = 0; i < styleColors.length; i++) {
-//           addArr.push(styleColors[i].dataset.color);   
-//         }
-//         window.localStorage.setItem('colors',JSON.stringify(addArr)) 
-//     };
-//     //get from local storage
-//     let getLocal=JSON.parse(window.localStorage.getItem('colors'));
-//     document.documentElement.style.setProperty('--bgColor-1',getLocal[0]);
-//     document.documentElement.style.setProperty('--bgColor-2',getLocal[1]);
-//     document.documentElement.style.setProperty('--primary-color',getLocal[2]);
-//     document.documentElement.style.setProperty('--light-color',getLocal[3]);
-//     });
-// function switchColors(cssColor,array,n) {
-//     document.documentElement.style.setProperty(cssColor,array[n].dataset.color);
-// }
-//#############################################################################################
-// scroll to top for website
+// scroll to top for website...........................................
 let up=document.querySelector('.up');
 window.onscroll=function () {
  if (window.scrollY>=300) {
     up.style.display="block"
 } else {
-    up.style.display="none"
-    
+    up.style.display="none" 
 }   
 }
 up.addEventListener('click',function () {
@@ -62,8 +14,7 @@ up.addEventListener('click',function () {
         behavior: "smooth"
     })
 })
-//#############################################################################
-// add class active to nav link.....
+// add class active to nav link......................................................
 let navLink=document.querySelectorAll('.nav-link');
 navLink.forEach(function (ele) {
     ele.addEventListener('click',function linkActive() {
@@ -75,31 +26,49 @@ navLink.forEach(function (ele) {
         ele.classList.add('navLink-active')
     })
 })
-//##################################################################################
-// mode
+// change mode....................................................
 let mode= document.querySelector('.mode')
 let ball= document.querySelector('.mode .ball')
-let change=true;
-mode.addEventListener('click',()=>{
-    ball.classList.toggle('move')
-     if (change) {
-         change=false;
-        setP('--bgColor-1','#ffffff')
-        setP('--bgColor-2','#e7e2e1')
-        setP('--light-color','#171a1c')
-        setP('--primary-color','#ff274b')
-     } 
-     else {
-        change=true;
-        setP('--bgColor-1','#171a1c')
-        setP('--bgColor-2','#22282a')
-        setP('--light-color','#ffffff')
-        setP('--primary-color','#ff274b')  
-     }
-})
-function setP(v,c) {
-    document.documentElement.style.setProperty(v,c)
+let aColor=[]
+  
+//get from local storage
+if (localStorage.getItem('websiteColor')) {
+    aColor=JSON.parse(localStorage.getItem('websiteColor'))
+    setP(aColor[0],aColor[1],aColor[2],aColor[3])
+    //ball.style.transition= 0+'s'
+    // خاصية تنطي لون الخلفية للعنصر الي اريدة
+    let m=window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
+    if(m==='rgb(255, 255, 255)') {
+        ball.classList.add('move') 
+     }  
 }
+//mode event
+mode.addEventListener('click',()=>{
+    // خاصية تنطي لون الخلفية للعنصر الي اريدة
+    let bodyBackground=window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
+    // change to light mode
+     if (bodyBackground=='rgb(23, 26, 28)'){
+        ball.classList.add('move')
+         aColor=[];
+         setP('#ffffff','#e7e2e1','#171a1c','#ff274b')
+     } 
+    // go back to dark mode
+     if(bodyBackground==='rgb(255, 255, 255)') {
+        ball.classList.remove('move')
+        aColor=[];
+        setP('#171a1c','#22282a','#ffffff','#ff274b') 
+     }
+     // add to local storage
+    localStorage.setItem('websiteColor',JSON.stringify(aColor))
+})
+function setP(c1,c2,c3,c4) {
+    document.documentElement.style.setProperty('--bgColor-1',c1)
+    document.documentElement.style.setProperty('--bgColor-2',c2)
+    document.documentElement.style.setProperty('--light-color',c3)
+    document.documentElement.style.setProperty('--primary-color',c4) 
+    aColor.push(c1,c2,c3,c4)
+}
+
 //################################################################################
 // let about=document.querySelector(".about");
 // let aboutText=document.querySelector(".about-text");
